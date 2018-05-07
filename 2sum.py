bd5844f686bb14baafb6de5200a03d96ce67ebeb
gabrielkerr <mgabrielkerr@gmail.com>
2018-05-07 11:28:36 -0600

    Refactor to run in linear time.

--- 2sum.py
+++ 2sum.py
@@ -3,40 +3,25 @@ Solution to 2 sum problem from dailycodingproblem.com
 """
 
 
-def two_sum(l, k, count, cache):
+def two_sum(l, k):
+    m = [0]*(max(l) + 1)
 
-    if count > 2:
-        return False
+    for x in l:
+        m[x] += 1
 
-    if k == 0:
-        return True
+    for x in l:
+        if k-x == x and m[k-x] >= 2:
+            return True
 
-    if k < 0:
-        return False
+        if m[k-x] != 0:
+            return True
 
-    sorted_l = sorted(l)
-    sorted_l.append(k)
-    t = tuple(sorted_l)
-    if t in cache.keys():
-        return cache[t]
-
-    has_soln = True
-
-    for i in range(len(l)):
-        curr = l[i]
-        val = has_soln and two_sum([x for x in l if x != curr], k-curr, count+1, cache)
-        if val:
-            break
-
-    cache[t] = val
-    return val
+    return False
 
 
 if __name__ == '__main__':
     l = [10, 15, 3, 7]
     k = 17
     k2 = 11
-    cache = {}
-    cache2 = {}
-    print(two_sum(l, k, 0, cache))
-    print(two_sum(l, k2, 0, cache2))
+    print(two_sum(l, k))
+    print(two_sum(l, k2))
